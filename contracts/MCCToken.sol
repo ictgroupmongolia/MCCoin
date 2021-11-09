9083ba343ecfd2808fa4d999b2270fd563da5f7d
Khashkhuu Gankhyag <xass1on@gmail.com>
2021-11-09 15:30:34 +0800

    update

--- contracts/MCCToken.sol
+++ contracts/MCCToken.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.9;
 
+// Author: Khashkhuu 'Xass1on' Gankhuyag
+
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/TokenTimelock.sol";
